243ef49e7840e3ef641cbd1c5941614acf21bbdc
gav-cooper <gavin.cooper@smail.astate.edu>
2023-04-28 01:35:54 -0500

    added profanity filter

--- Controllers/commentController.js
+++ Controllers/commentController.js
@@ -2,6 +2,9 @@
 
 const commentModel = require("../Models/commentModel");
 const usersModel = require("../Models/usersModel");
+const Filter = require("bad-words");
+
+const filter = new Filter();
 
 function addComment(req, res){
     if (!req.session.isLoggedIn) {
@@ -10,7 +13,11 @@ function addComment(req, res){
     if(req.session.user.username !== req.body.username)
         return res.sendStatus(400);
     const {userID} = usersModel.getUserByUsername(req.body.username);
-    const {musicID, message} = req.body;
+    const {musicID} = req.body;
+    let {message} = req.body;
+
+    message = filter.clean(message);
+    
     if (commentModel.addComment(musicID, message, userID))
         return res.sendStatus(200);
     else
